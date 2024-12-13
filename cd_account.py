3b725790a10deaa7f66e6ac45e601f02320fad1a
Dragon9804 <jason.smoody@yahoo.com>
2024-12-12 19:20:59 -0700

    Finished cd_account

--- cd_account.py
+++ cd_account.py
@@ -1,7 +1,7 @@
 """Import the Account class from the Account.py file."""
-# ADD YOUR CODE HERE
+from Account import Account
 
-def create_cd_account(balance, interest_rate, months):
+def create_cd_account():
     """Creates a CD account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -13,21 +13,30 @@ def create_cd_account(balance, interest_rate, months):
         float: The updated CD account balance after adding the interest earned.
         And returns the interest earned.
     """
+    balance = float(input("Enter the current CD account balance: "))
+    interest_rate = float(input("Enter the APR interest rate: "))
+    months = int(input("Enter the length of months for the CD: "))
+
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
-    # ADD YOUR CODE HERE
+    cd_account = Account(balance, interest_rate)
 
     # Calculate interest earned
-    # ADD YOUR CODE HERE
+    earned_interest = balance * (interest_rate / 100) * (months / 12)
 
     # Update the CD account balance by adding the interest earned
-    # ADD YOUR CODE HERE
+    balance += earned_interest
 
     # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
+    cd_account.set_balance(balance)
 
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
+    cd_account.set_interest(earned_interest)
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return  balance, earned_interest
+
+# Call the create_cd_account function and pass the required arguments.
+updated_balance, earned_interest = create_cd_account()
+print(f"Updated balance: ${updated_balance:.2f}")
+print(f"Interest earned: ${earned_interest:.2f}")
